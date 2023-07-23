6be5fe0cd25b2df5e2b3e54b7ce649203b8aa9a4
sagorahm <sagorahm25@gmail.com>
2023-07-23 21:57:05 +0600

    s

--- store/views.py
+++ store/views.py
@@ -1,4 +1,7 @@
+
+from aifc import Error
 from django.shortcuts import get_object_or_404, render
+from requests import request 
 
 # Create your views here.
 from . models import Product,Category
@@ -14,3 +17,15 @@ def store(request,category_slug=None):
         products = Product.objects.all().filter(is_available=True)
         product_count=products.count()
     return render (request,'store/store.html',{'products':products, 'p_count':product_count,'categories':categories})
+
+
+def product_details(request,category_slug,product_slug):
+    try:    
+        single_product=Product.objects.get(category__slug=category_slug,slug=product_slug)
+    except Exception  as e:
+        raise e
+    context={
+        'single_product':single_product
+    }
+    return render (request,'store/product_details.html',context)
+    
