e64f9b662dcbff5ec487f4e6dc0a317a6c488f03
sagorahm <sagorahm25@gmail.com>
2023-07-26 23:34:19 +0600

    s

--- djangomart/settings.py
+++ djangomart/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'accounts',
     'category',
     'store',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -67,7 +68,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'category.context_processors.menu_links'
+                'category.context_processors.menu_links',
+                'cart.context_processors.counter',
             ],
         },
     },
